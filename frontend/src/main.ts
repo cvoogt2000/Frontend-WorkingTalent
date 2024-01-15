e86b29edb3b7b3b3795f4c68fcb10bf4b71f911d
cvoogt2000 <cvoogt2000@gmail.com>
2024-01-15 12:33:09 +0100

    added a bookcover

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {providers: [provideClientHydration(), provideHttpClient(withFetch())]})
+  .catch((err) => console.error(err));
