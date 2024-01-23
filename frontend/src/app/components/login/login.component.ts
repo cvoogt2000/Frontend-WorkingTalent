9bf074eee5f611418cc6a0d21b4884ec9a60e6ea
cvoogt2000 <cvoogt2000@gmail.com>
2024-01-23 15:05:00 +0100

    reservation trying to get it to work

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public errorMessage: String = "";
 
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) { }
+  
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
+  submitLogin(): void {
+    this.userService.login(this.loginForm.value.email!, this.loginForm.value.password!)
+            .subscribe({
+                next: (data) => {
+                    localStorage.setItem("key", data.key);
+                    window.location.replace('/homepage');
+                },
+                error: (error) => {              
+                    if (error.error.message) {
+                        this.errorMessage = error.error.message;
+                    } else {
+
+                        this.errorMessage = error.error;               
+                    }
+                    this.loginForm.patchValue({ password: ''});
+                }
+            }
+        );
+  }
+
+  ngOnInit(): void {
+
+  }
 }
