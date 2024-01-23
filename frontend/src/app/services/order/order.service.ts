fd8849236298f22ac5e75c492d2ebe59c1cbeae3
martintielemans <martintielemans@hotmail.com>
2024-01-23 16:16:02 +0100

    Add Order History Admin

--- frontend/src/app/services/order/order.service.ts
+++ frontend/src/app/services/order/order.service.ts
@@ -5,9 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class OrderService {
   private orders: any[] = [
-    { startdate: '23-01-2024', enddate: '30-01-2024', product: 'Boek A', copy: '1', name: 'Tim Hammers', status: 'Missing'},
-    { startdate: '22-01-2024', enddate: '30-01-2024', product: 'Boek B', copy: '2', name: 'Bas de Nice', status: 'Missing'},
-    // Add more sample orders
+    
   ];
 
   getOrders(): any[] {
